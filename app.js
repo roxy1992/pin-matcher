7ffe51cb60ab08c399a48082e04b28cd428f6c50
roxy1992 <roxybhuiyan1992@gmail.com>
2022-02-13 15:57:01 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        //console.log('got 3 digit and calling again', pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    console.log(event.target.innerText);
+});
